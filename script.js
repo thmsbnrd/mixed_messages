0b9823dce3a6c7739262f4a47cfaa5e7fd37faa9
Thomas BENARD <thomas0benard@gmail.com>
2023-11-09 11:28:32 +0100

    add activities array

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const travelAdventure = {
   countries: ["Japan", "Brazil", "Italy", "Kenya", "Australia"],
+  activities: [
+    "savor the famous street food",
+    "visit a historical site",
+    "buy a unique souvenir gift",
+    "take a scenic hike",
+    "enjoy a local cooking class",
+  ],
 };
 
 console.log("test print to console");
